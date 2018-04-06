807890de9422829480606a8fe4391cf8b9ef5281
William Palermo <palermow@gmail.com>
2018-04-06 14:18:41 -0300

    server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -9,9 +9,9 @@ bodyParser = require('body-parser');
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/bluehunter', {useMongoClient:true}); 
+//mongoose.connect('mongodb://localhost/bluehunter', {useMongoClient:true}); 
 
-
+mongoose.connect("mongodb://wpalermo:1234@cluster0-ibm-shard-00-00-hogza.mongodb.net:27017,cluster0-ibm-shard-00-01-hogza.mongodb.net:27017,cluster0-ibm-shard-00-02-hogza.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-ibm-shard-0&authSource=admin" , {useMongoClient:true});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
